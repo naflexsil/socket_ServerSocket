ff8c4589cb4a4d04aa7570e764731624557be1da
a_лина <118667639+naflexsil@users.noreply.github.com>
2024-11-28 20:11:00 +0700

    Initial commit

---
+++ src/main/java/org/example/socket_serversocket/ChatServer.java
@@ -0,0 +1,11 @@
+package org.example.socket_serversocket;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient {
+    public static void main(String[] args) {
+
+    }
+}
